b92108a02631fa725c75df6da56b160da844f870
Seth Archer Brown <learc83@Seth-Browns-MacBook-Pro.local>
2011-03-14 00:21:23 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Labelr::Application.config.secret_token = '817ce8913bc478820d7c2145ebd33ebe760131d51ee01f398deda155d0a5fe8f6d19eaca02736c0fb91b3f649a322bd2c868819515cbf36bc1c4de843f9ab468'
