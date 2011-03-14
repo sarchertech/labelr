82b0910e4324941e9f000791957ebf0e608b73d6
Seth Archer Brown <learc83@Seth-Browns-MacBook-Pro.local>
2011-03-14 00:41:02 -0400

    added http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+  
+  protected
+  
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "toasty" && password == "gunner11"
+    end
+  end
 end
